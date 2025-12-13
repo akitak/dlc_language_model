d25668ed05e8358e5950679ff633e0a63d148516
akitak <akira.j.takahashi@gmail.com>
2025-12-13 09:41:22 +0100

    Completion of char.py, and tested in exploration notebook

--- char.py
+++ char.py
@@ -1,3 +1,4 @@
+import torch
 from torch.utils.data import Dataset
 
 class CharDataset(Dataset):
@@ -7,12 +8,18 @@ class CharDataset(Dataset):
     Adapted from "https://github.com/karpathy/minGPT".
     """
 
-    def __init__(self, config, data):
+    def __init__(self, block_size, data): # Going to define block_size in notebook above instantiation of CharDataset object when reading data / training model
+    # def __init__(self, config, data):
 
-        chars = sorted(list(set(data))) # get characters from the input data # IMPLEMENTED
+        self.data = data # IMPLEMENTED
+        self.block_size = block_size # IMPLEMENTED
+
+        chars = sorted(list(set(self.data))) # get characters from the input data # IMPLEMENTED
         self.stoi = { ch:i for i,ch in enumerate(chars) } # map characters to integer indices
         self.itos = { i:ch for i,ch in enumerate(chars) } # similarly, map integer to indices, necessary for decoding and prediction # IMPLEMENTED
         self.vocab_size = len(chars) # IMPLEMENTED
+        self.data_size = len(self.data) # IMPLEMENTED
+        
         
         ...
 
@@ -20,11 +27,13 @@ class CharDataset(Dataset):
         return self.vocab_size # IMPLEMENTED
 
     def __len__(self):
-        return len(self.
+        return self.data_size - self.block_size # IMPLEMENTED # Number of training samples using a sliding window of length block_size #TODO: IMPLEMENT Config
 
     def __getitem__(self, idx):
-        # grab a chunk of (block_size + 1) characters from the data
-        encode = lambda text: [self.stoi[char] for char in text] # encode every character to an integer # IMPLEMENTED # TODO: save as tensor
-        decode = lambda integers: ''.join([self.itos[integer] for integer in integers]) # decode every character to an integer # IMPLEMENTED # TODO: save as tensor
+        chunk = self.data[idx:idx+self.block_size+1]# grab a chunk of (block_size + 1) characters from the data
+        encoded_tensor = torch.tensor([self.stoi[c] for c in chunk], dtype=torch.long) # encode every character to an integer # IMPLEMENTED
+        # decode = lambda integers: ''.join([self.itos[integer] for integer in integers]) # decode every character to an integer # IMPLEMENTED
         # return the chunk and the shifted version as tensors
-        pass
+        x = encoded_tensor[:-1] # IMPLEMENTED
+        y = encoded_tensor[1:] # IMPLEMENTED
+        return x, y # IMPLEMENTED
